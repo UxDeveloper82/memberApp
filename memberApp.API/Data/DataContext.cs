32ebd29604e26997158f2c4e4c855eae63421b33
electronicsmf <electronicsmf@aol.com>
2018-12-09 19:59:27 -0500

    Initial Set up

---
+++ memberApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using memberApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace memberApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values {get; set;}
+    }
+}
---
+++ memberApp.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace memberApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }   
+    }
+}
