6e595efa12aed147826620392e7360733c529bc3
electronicsmf <electronicsmf@aol.com>
2018-12-11 16:15:04 -0500

    Create Routes --Protect routes with AuthGuards

--- memberApp-SPA/src/app/app.module.ts
+++ memberApp-SPA/src/app/app.module.ts
@@ -1,15 +1,21 @@
+import { AuthGuard } from './_guards/auth.guard';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
 
 
 @NgModule({
@@ -17,17 +23,22 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MessagesComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
